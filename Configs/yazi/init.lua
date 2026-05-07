6045cd0ca81904c76700e800e16c891fe60f7357
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-05-07 14:19:35 +0530

    Some keybinds added in mangoWM

--- Configs/yazi/init.lua
+++ Configs/yazi/init.lua
@@ -9,11 +9,11 @@ function Linemode:size_and_mtime()
 	if time == 0 then
 		time = ""
 	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
+		time = os.date("%d %B %H:%M", time)
 	else
 		time = os.date("%b %d  %Y", time)
 	end
 
 	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+	return string.format("%s %s", size and ya.readable_size(size) or "", time)
 end
